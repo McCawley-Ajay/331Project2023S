0e29c25a48ea728e7591750216225dbeb146f0ec
carsondiehl <carsonndiehl@gmail.com>
2023-12-05 02:11:44 -0700

    Added page, edited problematic files, edited nav

---
+++ Components/Pages/Userdata.cs
@@ -0,0 +1,10 @@
+public class UserData
+{
+    public string UserName { get; set; }
+
+    // Default constructor
+    public UserData()
+    {
+        UserName = string.Empty; // or another default value
+    }
+}
